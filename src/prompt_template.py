97262be3c5641874f906f58ff0124cce17f69284
PureLearning-dev <liu.jie.coder@gmail.com>
2026-02-08 21:01:11 +0800

    feat: 提示词模版

---
+++ src/prompt_template.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+
+from src.utils.models import deep_seek_v3_stream
+
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system", "Translate the following from English into {language}"),
+    ("user", "{text}")
+])
+
+# 这个判断主要是为了防止在引用这个文件时，导致里面的测试代码执行，造成不必要的错误
+if __name__ == "__main__":
+    load_dotenv()
+
+    prompt = prompt_template.invoke(
+        {
+            "language": "Chinese",
+            "text": "Hello, How are you? Fuck!"
+        }
+    )
+
+    deep_seek_v3_stream(prompt)
+
+
+
