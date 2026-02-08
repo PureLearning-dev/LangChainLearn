da794bbee0f040e17d55d4d4b23c1ccb27f4288f
PureLearning-dev <liu.jie.coder@gmail.com>
2026-02-08 21:00:49 +0800

    feat: deepseek大模型调用

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from dotenv import load_dotenv
 
-from src.models import deep_seek_mode
+from src.utils.models import deep_seek_v3_stream
+from src.prompt_template import prompt
 
 load_dotenv()
 
-response = deep_seek_mode(
-    "请翻译以下对话：hello, How are you?"
-)
+# message = input("请输入想问AI模型的问题：")
 
-print(response.content)
+# deep_seek_pro_stream(message)
+
+deep_seek_v3_stream(prompt)
+
+# chain_v3 = deep_seek_v3_chain()
+#
+# final_chain = prompt | chain_v3
+#
+# for chunk in final_chain:
+#     print(chunk)
+
--- src/models.py
+++
@@ -1,6 +0,0 @@
-from langchain_deepseek import ChatDeepSeek
-
-
-def deep_seek_mode(message):
-    llm = ChatDeepSeek(model="deepseek-chat")
-    return llm.invoke(message)
